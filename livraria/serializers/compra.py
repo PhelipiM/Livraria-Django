be9e30d8b2c36c9d3a73f24b193671118ad73790
PhelipiM <ggizmeumigo@gmail.com>
2023-09-05 14:26:40 -0300

    parte 29

--- livraria/serializers/compra.py
+++ livraria/serializers/compra.py
@@ -14,12 +14,18 @@ class ItensCompraSerializer(ModelSerializer):
         depth = 2
 
 class CompraSerializer(ModelSerializer):
-    class Meta:
-        model = Compra
-        fields = "__all__"
-
-class CompraSerializer(ModelSerializer):
     usuario = CharField(source="usuario.email", read_only=True)
     itens = ItensCompraSerializer(many=True, read_only=True)
+    status = CharField(source="get_status_display", read_only=True)
     
+    class Meta:
+        model = Compra
+        fields = ("id", "usuario", "status", "total", "itens")
 
+    @property
+    def total(self):
+        # total = 0
+        # for item in self.itens.all():
+        #     total += item.livro.preco * item.quantidade
+        # return total
+        return sum(item.livro.preco * item.quantidade for item in self.itens.all())
